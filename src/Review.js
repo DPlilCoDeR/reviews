bc04d37a1bd8b678c7dacfe0f7252391e1fb60b1
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-10 16:18:38 +0100

    elements of review component

--- src/Review.js
+++ src/Review.js
@@ -7,11 +7,21 @@ const Review = () => {
     const [peopleState, setPeopleState] = useState(people)
     console.log(peopleState)
     return (
-        <section>
-                {peopleState[positionArray].name} <br/>
-                {peopleState[positionArray].job}
-            <img src={peopleState[positionArray].image}/>
-            <p>{peopleState[positionArray].text}</p>
+        <section className='review'>
+                <FaQuoteRight className='quote-icon'/>
+                <div className='img-container'>
+                    <img className='person-img' src={peopleState[positionArray].image}/>
+                </div>
+                <h4 className='author'>
+                    {peopleState[positionArray].name}
+                </h4>
+                <p className='job'>
+                    {peopleState[positionArray].job}
+                </p>
+                <p className='info'>{peopleState[positionArray].text}</p>
+                <button className='prev-btn'><FaChevronLeft/></button>
+                <button className='next-btn'><FaChevronRight/></button>
+                <button className='random-btn'>Suprise Me!</button>
         </section>
     )
 };
