baa9fe99ea2ba59449774a6b4148eaeeab269056
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-10 15:49:56 +0100

    testing review component

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,17 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+    let positionArray = 0;
+    const [peopleState, setPeopleState] = useState(people)
+    console.log(peopleState)
+    return (
+        <section>
+                {peopleState[positionArray].name} <br/>
+                {peopleState[positionArray].job}
+            <img src={peopleState[positionArray].image}/>
+            <p>{peopleState[positionArray].text}</p>
+        </section>
+    )
 };
 
 export default Review;
