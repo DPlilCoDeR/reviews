955d824d1808767b693672b6646877ae5c7f8300
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-10 17:28:28 +0100

    add final styles to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Review from './Review';
 function App() {
-  
+
   return (
   <main>
-    <h2 className='title'>Ours reviews</h2>
-    
-    <Review/>
+    <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
   </main>
   );
 }
