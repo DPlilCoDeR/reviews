923d303768ad991fd867839e35ba46c07d47d532
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-10 17:26:42 +0100

    add randPosition

--- src/Review.js
+++ src/Review.js
@@ -3,25 +3,50 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-    let positionArray = 0;
-    const [peopleState, setPeopleState] = useState(people)
-    console.log(peopleState)
+    let initialPosition = 0;
+    const [position, setPosition] = useState(initialPosition)
+    const [peopleState, _setPeopleState] = useState(people);
+
+    const prevReview = () => {
+        if (position > initialPosition) {
+            let actualPosition = position;
+            let newPosition = -- actualPosition;
+            setPosition(newPosition);
+        }
+    };
+
+    const nextReview = () => {
+        if (position < peopleState.length -1) {
+            let actualPosition = position;
+            let newPosition = ++ actualPosition;
+            setPosition(newPosition);
+        }
+    };
+
+    const randReview = () => {
+        const randomPosition = Math.floor(Math.random() * peopleState.length);
+        if (randomPosition !== position) {
+            setPosition(randomPosition);
+        }
+    };
+
     return (
+        
         <section className='review'>
                 <FaQuoteRight className='quote-icon'/>
                 <div className='img-container'>
-                    <img className='person-img' src={peopleState[positionArray].image}/>
+                    <img className='person-img' src={peopleState[position].image}/>
                 </div>
                 <h4 className='author'>
-                    {peopleState[positionArray].name}
+                    {peopleState[position].name}
                 </h4>
                 <p className='job'>
-                    {peopleState[positionArray].job}
+                    {peopleState[position].job}
                 </p>
-                <p className='info'>{peopleState[positionArray].text}</p>
-                <button className='prev-btn'><FaChevronLeft/></button>
-                <button className='next-btn'><FaChevronRight/></button>
-                <button className='random-btn'>Suprise Me!</button>
+                <p className='info'>{peopleState[position].text}</p>
+                <button onClick={prevReview} className='prev-btn'><FaChevronLeft/></button>
+                <button onClick={nextReview} className='next-btn'><FaChevronRight/></button>
+                <button onClick={randReview} className='random-btn'>Suprise Me!</button>
         </section>
     )
 };
