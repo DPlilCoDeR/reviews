05b8c42270f4ea30cc5ad0ec55b087428466d97e
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-02-10 13:15:53 +0100

    us

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,8 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [reviews, setReviews] = useState(data)
+  const [value, setValue] = useState(0)
+
+  const slide = () => {
+    if(value <= reviews.length){
+      setValue(value + 1)
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(slide, 5000)
+  })
+
+  const {id, image, name, title, quote} = reviews[value];
+
+  return <main>
+    <section>
+      <div className='title'>
+        
+        <h2>Slider</h2>
+      </div>
+
+    </section>
+    <section className='section-center'>
+      <article>
+          <h4>{name}</h4>
+          <img className='person-img' src={image}/>
+          <div className='title'>{title}</div>
+          <div className='text'>{quote}</div>
+      </article>
+    </section>
+  </main>;
 }
 
 export default App;
