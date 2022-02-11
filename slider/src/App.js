2a7b41a28ec4cf7691ded616943fbc83711e5698
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-02-11 11:50:24 +0100

    slide funct works

--- slider/src/App.js
+++ slider/src/App.js
@@ -9,13 +9,17 @@ function App() {
   const [value, setValue] = useState(0)
 
   const slide = () => {
-    if(value <= reviews.length){
+    if (value == reviews.length - 1) {
+      setValue(0)
+      return
+    }
+    if(value <= reviews.length - 1){
       setValue(value + 1)
     }
   }
 
   useEffect(() => {
-    setTimeout(slide, 5000)
+    setTimeout(slide, 3000)
   })
 
   const {id, image, name, title, quote} = reviews[value];
