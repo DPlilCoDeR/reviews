60428dae150c3f2a01cf9dd2d5e9c518eb349ced
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-02-11 12:37:56 +0100

    try to implements slide animation

--- slider/src/App.js
+++ slider/src/App.js
@@ -8,39 +8,71 @@ function App() {
   const [reviews, setReviews] = useState(data)
   const [value, setValue] = useState(0)
 
-  const slide = () => {
-    if (value == reviews.length - 1) {
-      setValue(0)
-      return
-    }
-    if(value <= reviews.length - 1){
-      setValue(value + 1)
-    }
-  }
 
   useEffect(() => {
-    setTimeout(slide, 3000)
-  })
+    const lastIndex = reviews.length - 1;
+    if (value < 0) {
+      setValue(lastIndex);
+    }
+    if (value > lastIndex) {
+      setValue(0);
+    }
+  }, [value, reviews]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setValue(value + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [value]);
 
   const {id, image, name, title, quote} = reviews[value];
 
-  return <main>
-    <section>
+  return (
+  <main>
+    <section className='section'>
       <div className='title'>
-        
-        <h2>Slider</h2>
+        <h2>
+          <span>/</span>Slider
+        </h2>
       </div>
 
+      <div className='section-center'>
+        {reviews.map((review, index) => {
+          const {id, image, name, title, quote} = review
+
+          let position = 'nextSlide';
+          if (index === value) {
+            position = 'activeSlide';
+          }
+          if (
+            index === value - 1 ||
+            (value === 0 && value === reviews.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+          return (
+            <article className={position} key={id}>
+              <img className='person-img' src={image} alt={name}/>
+              <h4>{name}</h4>
+              <div className='title'>{title}</div>
+              <div className='text'>{quote}</div>
+              <FaQuoteRight className='icon'/>
+            </article>
+          )
+        })} 
+            <button className='prev' onClick={() => setValue(value - 1)}>
+              <FiChevronLeft/>
+            </button>
+            <button className='next' onClick={() => setValue(value + 1)}>
+              <FiChevronRight/>
+            </button>
+      </div>
     </section>
-    <section className='section-center'>
-      <article>
-          <h4>{name}</h4>
-          <img className='person-img' src={image}/>
-          <div className='title'>{title}</div>
-          <div className='text'>{quote}</div>
-      </article>
-    </section>
-  </main>;
+  </main>
+  )
 }
 
 export default App;
